e3445c7dde55a918050485dac0db3426c12b4b7d
pablorodri <pablorodri.r@gmail.com>
2022-03-29 09:23:29 +0200

    copia en github

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <div>
+     <h4>Pablo</h4>
+     <ul>
+       <li>Rodríguez</li>
+       <li>Ramírez</li>
+       <li className='web'>Web Developer</li>
+     </ul>
+   </div>
+   </>
+  );
+}
+
+export default App;
